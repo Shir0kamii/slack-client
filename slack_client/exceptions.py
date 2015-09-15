a4e402caf7b5a90607b6a206046c96c53a37e860
Shir0kamii <shir0kamii@gmail.com>
2015-09-15 03:27:50 +0200

    Add a custom exception (SlackMissingAPI)
    This exception is raised when a SlackObject is created without
    any way to get an API.
    
    Remove custom implementation for exceptions. There is no reason strong
    enough for this.

--- slack_client/exceptions.py
+++ slack_client/exceptions.py
@@ -2,16 +2,7 @@ class SlackError(Exception):
     pass
 
 class SlackNo(SlackError):
-    def __init__(self, msg_error):
-        self.msg = msg_error
+    pass
 
-    def __str__(self):
-        return repr(self.msg)
-
-class SlackTooManyRequests(SlackError):
-    def __init__(self, time_to_wait):
-        self.time_to_wait = time_to_wait
-
-    def __str__(self):
-        return ("Too many requests. Wait %d seconds before trying again" \
-                % (self.time_to_wait))
+class SlackMissingAPI(SlackError):
+    pass
